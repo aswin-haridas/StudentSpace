4fce36389f6d04871a7f6cd15b97b13375ccb4f2
Aswin <67360184+aswinsoap@users.noreply.github.com>
2023-04-07 20:43:02 +0530

    Add files via upload

--- Flask SignupLogin/app.py
+++ Flask SignupLogin/app.py
@@ -1,6 +1,11 @@
 from flask import Flask , render_template , url_for
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECTET_KEY'] = 'secretkey'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
