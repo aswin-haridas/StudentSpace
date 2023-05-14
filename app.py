b05c6de49ccbb0bf06aae119dc3160cccd335259
aswinsoap <aswinth54@gmail.com>
2023-05-15 01:10:52 +0530

    oothappam veno penne bonda venoo

--- app.py
+++ app.py
@@ -1,36 +1,54 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request ,redirect, url_for
 import openpyxl
 import sqlite3
 
 app = Flask(__name__)
 
-@app.route('/')
-def upload():
-    return render_template('upload.html')
 
-@app.route('/process', methods=['POST'])
+@app.route("/")
+def upload():
+    return render_template("upload.html")
+
+
+@app.route("/process", methods=["POST"])
 def process():
-    file = request.files['file']
+    file = request.files["file"]
     wb = openpyxl.load_workbook(file)
     sheet = wb.active
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect("database.db")
     cursor = conn.cursor()
-    cursor.execute('''
+    cursor.execute(
+        """
         CREATE TABLE IF NOT EXISTS attendance (
             date TEXT,
             student_id TEXT,
             attendance_status INTEGER
         )
-    ''')
+    """
+    )
     for row in sheet.iter_rows(min_row=2):
         date = row[0].value
         student_id = row[1].value
         attendance_status = row[2].value
-        cursor.execute('INSERT INTO attendance VALUES (?, ?, ?)', (date, student_id, attendance_status))
+        cursor.execute(
+            "INSERT INTO attendance VALUES (?, ?, ?)",
+            (date, student_id, attendance_status),
+        )
     conn.commit()
     conn.close()
-    return 'File uploaded successfully!'
+    message = 'File uploaded successfully!'
+    return render_template('upload.html', message=message)
 
 
-if __name__ == '__main__':
+@app.route("/attendance")
+def attendance():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM attendance")
+    attendance_data = cursor.fetchall()
+    conn.close()
+    return render_template("attendance.html", attendance_data=attendance_data)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
