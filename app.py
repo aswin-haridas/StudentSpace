68ca6a0ee92865f044c946e8345012e04d9dfbbb
aswinsoap <aswinth54@gmail.com>
2023-06-06 14:00:47 +0530

    y

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
 from openpyxl import load_workbook
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
 
-import sqlite3
-
-
-import sqlite3
-from flask import Flask, render_template, request, redirect, url_for
-
-app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -26,39 +20,83 @@ def login():
 
         conn = sqlite3.connect("database.db")
         cursor = conn.cursor()
-        cursor.execute("SELECT role, name FROM users WHERE username=? AND password=?", (username, password))
+        cursor.execute(
+            "SELECT id, role, username FROM users WHERE username=? AND password=?",
+            (username, password),
+        )
         result = cursor.fetchone()
+
         if result is not None:
-            fetched_user_type, fetched_name = result
+            user_id, fetched_user_type, name = result
+            session["user_id"] = user_id
+            session["username"] = username
+            session["name"] = name
+
             if user_type == fetched_user_type:
-                return redirect(url_for("home", username=username, name=fetched_name, user_type=fetched_user_type))
+                return redirect(
+                    url_for(
+                        "home",
+                        username=username,
+                        id=user_id,
+                        name=name,
+                        user_type=fetched_user_type,
+                    )
+                )
+
         error_message = "Invalid username or password"
-        return render_template("login.html", error=error_message, admin=admin, faculty=faculty, student=student)
+        return render_template(
+            "login.html",
+            error=error_message,
+            admin=admin,
+            faculty=faculty,
+            student=student,
+        )
 
     return render_template("login.html", admin=admin, faculty=faculty, student=student)
 
 
 @app.route("/home/<username>/<name>/<user_type>", endpoint="home")
 def home(username, name, user_type):
-    return render_template("home.html", username=username, name=name, user_type=user_type)
+    id = request.args.get("id")
+    fullname = ""
 
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    if user_type == "student":
+        cursor.execute("SELECT name FROM studentlist WHERE id=?", (id,))
+    elif user_type == "faculty":
+        cursor.execute("SELECT name FROM facultylist WHERE id=?", (id,))
+
+    result = cursor.fetchone()
+    if result is not None:
+        fullname = result[0]
+
+    conn.close()
+
+    return render_template(
+        "home.html",
+        username=username,
+        name=name,
+        user_type=user_type,
+        fullname=fullname,
+    )
+
+
+@app.route("/grades")
+def grades(id):
+    conn = sqlite3.connect("database.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM grades WHERE id=?", (id,))
+    data = c.fetchall()
+    conn.close()
+    data = [[item if item is not None else None for item in row] for row in data]
+    return jsonify(data)
 
 
 @app.route("/profile")
 def profile():
     return render_template("profile.html")
-
-
-@app.route("/dashboard")
-def dashboard():
-    labels = ["January", "February", "March", "April", "May", "June"]
-    values = [10, 20, 30, 40, 50, 60]
-    return render_template("home.html")
-
-
-@app.route("/grades")
-def grades():
-    return render_template("grades.html")
 
 
 @app.route("/upload")
--- workarea.py
+++ workarea.py
@@ -1,33 +1,21 @@
 import csv
 import sqlite3
 
-# Connect to the database
-conn = sqlite3.connect("database.db")
-cursor = conn.cursor()
+# Connect to the SQLite database
+conn = sqlite3.connect('database.db')
+c = conn.cursor()
 
-# Create the studentlist table if it doesn't exist
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS studentlist (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        dob TEXT,
-        email TEXT,
-        course TEXT,
-        contact TEXT,
-        address TEXT
-    )
-""")
+# Create a table called "students"
+c.execute('''CREATE TABLE IF NOT EXISTS facultylist
+             (id INTEGER, name TEXT, dob TEXT, email TEXT, course TEXT, contact TEXT, address TEXT)''')
 
-# Read the CSV file and insert the data into the studentlist table
-with open("students.csv", "r") as file:
-    reader = csv.reader(file)
+# Read the CSV file and insert the data into the table
+with open('students.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
     next(reader)  # Skip the header row
+
     for row in reader:
-        id, name, dob, email, course, contact, address = row
-        cursor.execute(
-            "INSERT INTO studentlist (id, name, dob, email, course, contact, address) VALUES (?, ?, ?, ?, ?, ?, ?)",
-            (id, name, dob, email, course, contact, address)
-        )
+        c.execute("INSERT INTO facultylist VALUES (?, ?, ?, ?, ?, ?, ?)", row)
 
 # Commit the changes and close the connection
 conn.commit()
