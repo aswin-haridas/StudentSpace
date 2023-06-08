013c50fbda5b32ef32832286a6330029598ad3ae
Antony <tonypjuby@gmail.com>
2023-06-08 16:21:38 +0530

    commit from antony

--- workarea.py
+++ workarea.py
@@ -1,34 +1,14 @@
+import pandas as pd
 import sqlite3
-import csv
 
-# Connect to the SQLite database
+# Read the Excel file into a pandas DataFrame
+df = pd.read_excel('attendance.xlsx')
+
+# Establish a connection to the database
 conn = sqlite3.connect('database.db')
-cursor = conn.cursor()
 
-# Create the "grade" table if it doesn't exist
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS grade (
-        id INTEGER PRIMARY KEY,
-        ip INTEGER,
-        ds INTEGER,
-        dbms INTEGER,
-        algo INTEGER,
-        os INTEGER
-    )
-''')
+# Write the DataFrame to a database table
+df.to_sql('your_table_name', conn, if_exists='replace', index=False)
 
-# Read the CSV file and insert data into the "grade" table
-with open('student.csv', 'r') as file:
-    csv_data = csv.reader(file)
-    next(csv_data)  # Skip the header row
-
-    for row in csv_data:
-        id, ip, ds, dbms, algo, os = row
-        cursor.execute('''
-            INSERT INTO grade (id, ip, ds, dbms, algo, os)
-            VALUES (?, ?, ?, ?, ?, ?)
-        ''', (id, ip, ds, dbms, algo, os))
-
-# Commit the changes and close the connection
-conn.commit()
+# Close the database connection
 conn.close()
